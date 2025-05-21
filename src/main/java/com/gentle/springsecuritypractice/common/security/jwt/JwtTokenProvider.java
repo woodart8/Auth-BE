5afc0838ee39f65bdfbcd784073bd733e2809535
woodart8 <kibeom1145@gmail.com>
2025-05-21 15:03:23 +0900

    Refactor: yml기반으로 redis에 토큰 등록되게 수정

--- src/main/java/com/gentle/springsecuritypractice/common/security/jwt/JwtTokenProvider.java
+++ src/main/java/com/gentle/springsecuritypractice/common/security/jwt/JwtTokenProvider.java
@@ -8,7 +8,6 @@ import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
